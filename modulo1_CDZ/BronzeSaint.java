8446eacfae298592749b53df9df809ea06a6ca14
Luís Robinson <luis.robinson@cwi.com.br>
2017-04-17 17:07:05 -0300

    feat(BronzeSaint):  implementar classepara armaduras de bronze.

---
+++ modulo1_CDZ/BronzeSaint.java
@@ -0,0 +1,12 @@
+
+
+public class BronzeSaint extends Saint
+{
+    //filho tem que ter os mesmos contrutores do pai
+    public BronzeSaint(String nome,Armadura armadura) throws Exception{
+        super(nome,armadura);
+        this.qtdSentidosDespertados = 5;
+    }
+   
+   
+}
