da544e2e121437cbc46b0635b543000e387b7830
Luís Robinson <luis.robinson@cwi.com.br>
2017-04-17 16:37:11 -0300

    feat(Saint):  adicionar novas regras para a quantidade de sentidos de cada cavaleiro por armadura.

--- modulo1_CDZ/Saint.java
+++ modulo1_CDZ/Saint.java
@@ -5,12 +5,30 @@ public class  Saint {
     private double vida = 100.0;
     private Genero genero = Genero.NAO_INFORMADO;
     private Status status = Status.VIVO;
-    private int qtsSentidosDespertados = 5;
+    private int qtdSentidosDespertados = 4;
     
     //construtor
-    public Saint(String nome, Armadura armadura){
+    public Saint(String nome, Armadura armadura) throws Exception{
         this.nome = nome;
         this.armadura = armadura;
+        
+          if (this.armadura.getCategoria() == Categoria.PRATA) {
+            this.qtdSentidosDespertados = 6;
+        } else if (this.armadura.getCategoria() == Categoria.OURO) {
+            this.qtdSentidosDespertados = 7;
+            String constelacao = armadura.getConstelacao();
+            if ( !constelacao.equals("Áries") && !constelacao.equals("Touro") && !constelacao.equals("Escorpião") 
+            && !constelacao.equals("Virgem")
+            && !constelacao.equals("Peixes")
+            && !constelacao.equals("Câncer")
+            ) {
+                // dar erro
+                throw new Exception("Constelação inválida");
+            }
+        } else {
+            // bronze
+            this.qtdSentidosDespertados = 5;
+        }
     }
     
     public void vestirArmadura(){
@@ -45,8 +63,8 @@ public class  Saint {
         return this.armadura.getValorArmadura();
     }
     
-    public int getQtsSentidosDespertados(){
-        return this.qtsSentidosDespertados;
+    public int getQtdSentidosDespertados(){
+        return this.qtdSentidosDespertados;
     }
     
 }
