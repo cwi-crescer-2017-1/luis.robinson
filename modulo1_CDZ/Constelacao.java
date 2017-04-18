8d40a417d2765cea696d0f3f02c79ce6f87408b7
Luís Robinson <luis.robinson@cwi.com.br>
2017-04-18 17:38:41 -0300

    Feat(Constelacao): converter lista estática para dinâmica.

--- modulo1_CDZ/Constelacao.java
+++ modulo1_CDZ/Constelacao.java
@@ -1,9 +1,11 @@
 
+import java.util.ArrayList;
 
 public class Constelacao
 {
    private String nome;
-   private Golpe[] golpes = new Golpe[3];
+   //private Golpe[] golpes = new Golpe[3];
+   private ArrayList<Golpe> golpes = new ArrayList<>();
    private int ultimaPosicaoPreenchida = 0;
 
     public Constelacao(String nome) {
@@ -14,12 +16,12 @@ public class Constelacao
         return this.nome;
     }
     
-    public Golpe[] getGolpes() {
+    public ArrayList<Golpe> getGolpes() {
         return this.golpes;
     }
     
     public void adicionarGolpe(Golpe golpe) {
-        golpes[ultimaPosicaoPreenchida++] = golpe;        
+        golpes.add(golpe);        
     }
    
    
