d3acbe594bf16e3789d81f41483b70bb8c81580d
Luís Robinson <luis.robinson@cwi.com.br>
2017-04-18 14:25:36 -0300

    feat(Golpe): implementar métodos para golpe.

--- modulo1_CDZ/Golpe.java
+++ modulo1_CDZ/Golpe.java
@@ -4,4 +4,17 @@ public class Golpe
 {
    private String nome;
    private int fatorDano;
+   
+   public Golpe(String nome,int fatorDano){
+       this.nome = nome;
+       this.fatorDano = fatorDano;
+   }
+   
+   public String getNome(){
+       return this.nome;
+   }
+   
+   public Integer getFatorDano(){
+       return this.fatorDano;
+   }
 }
