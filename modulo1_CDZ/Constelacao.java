ec720bed2109c3fd8e642d2416b2c1b3325fb5ad
Luís Robinson <luis.robinson@cwi.com.br>
2017-04-19 14:22:29 -0300

    refactor(Constelacao): remover int não utilizado

--- modulo1_CDZ/Constelacao.java
+++ modulo1_CDZ/Constelacao.java
@@ -6,8 +6,7 @@ public class Constelacao
    private String nome;
    //private Golpe[] golpes = new Golpe[3];
    private ArrayList<Golpe> golpes = new ArrayList<>();
-   private int ultimaPosicaoPreenchida = 0;
-
+   
     public Constelacao(String nome) {
         this.nome = nome;
     }
