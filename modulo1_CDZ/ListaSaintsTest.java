415d5fd984432ac6c2ce7f994b22f563a579a85e
Luís Robinson <luis.robinson@cwi.com.br>
2017-04-19 15:28:57 -0300

    test(SaintTest): adicionar novas situações de teste.

--- modulo1_CDZ/ListaSaintsTest.java
+++ modulo1_CDZ/ListaSaintsTest.java
@@ -9,10 +9,33 @@ import java.util.List;
 
 public class ListaSaintsTest
 {
-     @Test
-    public void buscarPorNomeSemRegistroRetornaNull() {
+     @Test public void buscarSaintExistentePorNome() throws Exception {
         ListaSaints lista = new ListaSaints();
-        assertEquals(null, lista.buscarPorNome("Cacius"));
+        Saint june = new Saint("June", new Armadura(new Constelacao("Camaleão"), Categoria.BRONZE));
+        lista.adicionar(june);
+        assertEquals(june, lista.buscarPorNome("June"));
+    }
+
+    @Test public void buscarSaintExistenteComRepeticaoDeNomes() throws Exception {
+        ListaSaints lista = new ListaSaints();
+        Saint june = new Saint("June", new Armadura(new Constelacao("Camaleão"), Categoria.BRONZE));
+        Saint june2 = new SilverSaint("June", new Armadura(new Constelacao("Camaleão 2"), Categoria.PRATA));
+        lista.adicionar(june2);
+        lista.adicionar(june);
+        assertEquals(june2, lista.buscarPorNome("June"));
+    }
+
+    @Test public void buscarSaintInexistente() throws Exception {
+        ListaSaints lista = new ListaSaints();
+        Saint june = new Saint("June", new Armadura(new Constelacao("Camaleão"), Categoria.BRONZE));
+        Saint june2 = new SilverSaint("June", new Armadura(new Constelacao("Camaleão 2"), Categoria.PRATA));
+        lista.adicionar(june2);
+        lista.adicionar(june);
+        assertNull(lista.buscarPorNome("San Junipero"));
+    }
+
+    @Test public void buscarSaintComListaVazia() {
+        assertNull(new ListaSaints().buscarPorNome("Seiya"));
     }
     
     
