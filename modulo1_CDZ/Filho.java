a3c0e34a5289646bf0dd68453f4426e4060326be
Luís Robinson <luis.robinson@cwi.com.br>
2017-04-26 09:31:33 -0300

    feat(Family):  implementar  classes com herança e abstração.

---
+++ modulo1_CDZ/Filho.java
@@ -0,0 +1,34 @@
+import java.util.Optional;
+
+public class Filho extends Pai {
+    
+    {
+        System.out.println("Oi");
+    }
+    
+    public Filho(String versao1, int numero) {
+    }
+    
+    public Filho(int versao2) {
+        this("Valor padrão", versao2);
+    }
+    
+    public Filho() {
+    }
+    
+    public void deveExistirNaSubClasse() {
+        {
+            int a = 10;
+        }
+        // não consigo acessar
+        // System.out.println(a);
+    }
+
+    public void deveExitirSoNaFilho() {
+    }
+
+    public void imprime() {
+        super.imprime();
+        System.out.println("Filho");
+    }
+}
