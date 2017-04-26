d3f9569d21826b16c161cbc406b02b23d9659f09
Luís Robinson <luis.robinson@cwi.com.br>
2017-04-26 15:50:55 -0300

    fet(GoldSaint): adicionar tratamento com exceptions.

--- modulo1_CDZ/GoldSaint.java
+++ modulo1_CDZ/GoldSaint.java
@@ -1,15 +1,12 @@
+public final class GoldSaint extends Saint {
 
-public class GoldSaint extends Saint
-{
-   public GoldSaint(String nome, String nomeConstelacao)throws Exception{        
-        super(nome, new Armadura(new Constelacao(nomeConstelacao), Categoria.OURO));
-        this.qtdSentidosDespertados = 7;
-        validarConstelacao(nomeConstelacao);
-        
-    }
-    
-    public void validarConstelacao(String constelacao)throws Exception{
-        
+    public GoldSaint(String nome, String constelacao) throws Exception {
+        super(nome, new Armadura(new Constelacao(constelacao), Categoria.OURO));
+        try {
+            this.qtdSentidosDespertados = 7;
+        } catch (Exception e) {
+            System.out.println("Erro de atribuição dos sentidos");
+        }
         if ( !constelacao.equals("Áries") 
         && !constelacao.equals("Touro")
         && !constelacao.equals("Gêmeos")
@@ -23,7 +20,15 @@ public class GoldSaint extends Saint
         && !constelacao.equals("Aquário")
         && !constelacao.equals("Peixes")) {
             // dar erro
-            throw new Exception("Constelação inválida");
+            throw new ConstelacaoInvalidaException("Constelação inválida");
         }
+    }
+
+    public void moverNaVelocidadeDaLuz() {
+        System.out.println(this.getNome() + " na velocidade da Luz!");
+    }
+
+    public String getNome() {
+        return super.getNome() + "!!!!";
     }
 }
