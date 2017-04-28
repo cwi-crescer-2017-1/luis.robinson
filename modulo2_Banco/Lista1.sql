93289888c3ccce55c68f6659cf77336ed9b03011
Luís Robinson <luis.robinson@cwi.com.br>
2017-04-28 14:29:39 -0300

    feat(Lista1): exercícios da lista 1

---
+++ modulo2_Banco/Lista1.sql
@@ -0,0 +1,40 @@
+
+-- 1
+Create table CIDADEAUX(
+	ID INT IDENTITY NOT NULL,
+	NOME VARCHAR(30),
+	UF VARCHAR(2),
+	CONSTRAINT  PK_CIDADEAUX PRIMARY KEY (ID)
+);
+
+-- 2
+TRUNCATE TABLE CIDADEAUX;
+
+select * 
+  into cidadeAux 
+  from cidade c 
+  where c.UF = 'RS';
+-- 3
+
+Create table Produto(
+	id int identity not null,
+	nome_curto varchar(20),
+	nome_descricao varchar(50),
+	data_descricao date,
+	local_estoque varchar(20),
+	quantidade decimal(14,4),
+	preco decimal(16,2),
+	constraint PK_IDPRODUTO primary key (id)
+	)
+
+	
+-- 4
+insert 
+  into produto (nome_curto,nome_descricao,data_descricao,local_estoque,quantidade,preco) 
+  values ('teste1','teste grande 1','28/04/2017','frente da loja',1.0000,15.90);
+
+insert 
+  into produto (nome_curto,nome_descricao,data_descricao,local_estoque,quantidade,preco) 
+  values ('teste2','teste grande 2','30/04/2017','fundo da loja',3.0000,22.87);
+
+  select * from produto
