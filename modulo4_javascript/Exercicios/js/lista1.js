ef857f2a03721a6f54e73d93dd542e8c339c92e1
Luís Robinson <luis.robinson@cwi.com.br>
2017-05-12 01:20:17 -0300

    feat(Lista1): implementar exercício 1 e 2 da lista.

---
+++ modulo4_javascript/Exercicios/js/lista1.js
@@ -0,0 +1,35 @@
+function daisyGame (number){
+  if (number %2 == 0){
+    console.log("Love me not");
+  } else {
+    console.log("Love me!");
+  }
+};
+
+//daisyGame(3);
+//daisyGame(4);
+
+function maiorTexto(Stringao){
+  var maior = "UAU";
+  for (var i = 0;i < Stringao.length; i++){
+    if (Stringao[i].length > maior.length){
+      maior = Stringao[i];
+    }
+  }
+  console.log("The big one is:",maior);
+}
+
+//var fruits = ["Banana", "Orange", "Apple", "Mango"];
+//maiorTexto(fruits);
+
+var instrutor = [ 'bernardo', 'nunes', 'fabrício', 'ben-hur', 'carlos' ] ;
+
+function arrayInstrutor(instrutor) {
+   console.log('olá querido instrutor:', instrutor)
+ };
+
+function imprime(instrutor, funcao){
+  if (typeoff funcao !== function){
+
+  }
+};
