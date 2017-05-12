3d9d23bccc09700bf6f5ffe23d988423e1377429
Luís Robinson <luis.robinson@cwi.com.br>
2017-05-12 15:49:30 -0300

    refactor(Lista1): Alterar primeiro exercício, para que seja melhor escrito.

--- modulo4_javascript/Exercicios/js/lista1.js
+++ modulo4_javascript/Exercicios/js/lista1.js
@@ -1,9 +1,6 @@
 function daisyGame (number){
-  if (number %2 == 0){
-    console.log("Love me not");
-  } else {
-    console.log("Love me!");
-  }
+  return number %2 == 0 ? 'Love me not' : 'Love me!'
+  //  return 'Love me${ numero %2 !== 0 ? '' : ' not'}'
 };
 
 //daisyGame(3);
@@ -30,6 +27,7 @@ function funcInstrutor(instrutor) {
 
 function imprime(instrutor, funcao){
   if (typeof funcao !== 'function'){
+
     console.log("Não é uma function");
   } else {
     for (var i = 0;i < instrutor.length; i++){
@@ -44,12 +42,13 @@ function somar (a){
   return function somar (b) {
     return a+b;
   }
-}
+};
 //somar(3);
 //somar(4);
 //console.log("somar",somar(3)(4));
 
 function fiboSum (number) {
+
    return number <= 1 ? number
         // viva lá recursividad.
         : fiboSum(number - 1) + fiboSum(number - 2);
