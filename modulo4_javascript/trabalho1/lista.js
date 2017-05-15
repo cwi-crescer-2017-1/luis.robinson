9431d3c8f42376246a018ff74198a1953498eda7
Luís Robinson <luis.robinson@cwi.com.br>
2017-05-15 14:39:02 -0300

    feat(lista.js): implementar solução do instrutor.

--- modulo4_javascript/trabalho1/lista.js
+++ modulo4_javascript/trabalho1/lista.js
@@ -11,6 +11,22 @@ function seriesInvalidas(series){
 };
 
 console.log(seriesInvalidas(series));
+//for in , propriedades do objeto
+// Ex 01
+// 1. criar funcao para verificar se série é inválida
+// 2. aplicar criterio de verificação de série inválida para as séries originais
+// 3. retornar string resultante de acordo com o formato esperado
+function seriesInvalidas1(series) {
+  let invalidas = series.filter(serie => {
+    // for (let campo in serie) { }
+    let algumCampoInvalido = Object.values(serie).some(v => v === null || typeof v === 'undefined');
+    let estreiaInvalida = serie.anoEstreia > new Date().getFullYear();
+    return estreiaInvalida || algumCampoInvalido;
+  });
+  return `Séries Inválidas: ${ invalidas.map(s => s.titulo).join(" - ") }`;
+}
+
+//
 
 //Exer 2
 var filtrarSeriesPorAno = (series, ano) =>
