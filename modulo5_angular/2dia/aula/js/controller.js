63ef9f55c14cc2d2d594c4e6c26bb8d6a95a643e
Luís Robinson <luis.robinson@cwi.com.br>
2017-05-18 17:23:20 -0300

    feat(aula2): implementar exercícios.

--- modulo5_angular/2dia/aula/js/controller.js
+++ modulo5_angular/2dia/aula/js/controller.js
@@ -1,11 +1,13 @@
 var modulo = angular.module('aula02', []);
 
+
 modulo.controller('Exemplo01', function ($scope, $filter) {
 
   //console.log($locale);
 
   //$scope.dataDigitada = document.getElementsByName("data").value;
   $scope.converter = converter;
+
 
   function converter() {
     if (angular.isUndefined($scope.dataDigitada)) {
@@ -20,7 +22,53 @@ modulo.controller('Exemplo01', function ($scope, $filter) {
   let dataObjeto = new Date(dataFormatada);
 
   $scope.dataObjeto = dataObjeto;
-  $scope.dataPronta = $filter('date')(dataObjeto, 'mediumDate')
+  $scope.dataPronta = $filter('date')(dataObjeto, 'shortDate');
+
+
 }
 
+
+var instrutores = [{
+    nome: 'Bernardo',
+    aula: [{
+        numero: 1,
+        nome: 'OO'
+      },
+      {
+        numero: 4,
+        nome: 'Javascript'
+      }
+    ]
+  },
+  {
+    nome: 'Nunes',
+    aula: [{
+      numero: 2,
+      nome: 'Banco de Dados I'
+    }]
+  },
+  {
+    nome: 'Pedro (PHP)',
+    aula: [{
+      numero: 3,
+      nome: 'HTML e CSS'
+    }]
+  },
+  {
+    nome: 'Zanatta',
+    aula: [{
+      numero: 5,
+      nome: 'AngularJS'
+    }]
+  }
+];
+
+$scope.instrutores = instrutores;
+
+});
+
+modulo.filter('mascada',function(){
+  return function(nome){
+        return nome.replace(/Nunes/i,'$ Nunes $');
+  }
 });
