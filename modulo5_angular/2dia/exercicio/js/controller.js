ed7afce8d8c8b01f0861502c0c5cca3f178b0bad
Luís Robinson <luis.robinson@cwi.com.br>
2017-05-19 11:48:44 -0300

    feat(Tema2): implementar segundo tema.

---
+++ modulo5_angular/2dia/exercicio/js/controller.js
@@ -0,0 +1,65 @@
+
+var modulo = angular.module('Aula02',[]);
+
+
+modulo.controller('instrutoresController',function($scope){
+
+
+  var instrutores = [{
+      nome: 'Bernardo',
+      aula: [{
+          numero: 1,
+          nome: 'OO'
+        },
+        {
+          numero: 4,
+          nome: 'Javascript'
+        }
+      ]
+    },
+    {
+      nome: 'Nunes',
+      aula: [{
+        numero: 2,
+        nome: 'Banco de Dados I'
+      }]
+    },
+    {
+      nome: 'Pedro (PHP)',
+      aula: [{
+        numero: 3,
+        nome: 'HTML e CSS'
+      }]
+    },
+    {
+      nome: 'Zanatta',
+      aula: [{
+        numero: 5,
+        nome: 'AngularJS'
+      }]
+    }
+  ];
+			let orderAula  = []
+
+			instrutores.forEach( instrutor =>
+				instrutor.aula.forEach(aula=>
+					orderAula.push({numero : aula.numero, aula : aula.nome ,nome : instrutor.nome }
+			)))
+
+
+
+			$scope.instrutores = orderAula;
+})
+
+modulo.filter('filtroAulaInstrutor',function(){
+	return function(instrutor){
+
+// se ins > 10 só precisa de um 0.
+		let lpad = instrutor.numero > 10 ? '0' : '00'
+		let numero = lpad + instrutor.numero
+		let aula = instrutor.aula.toUpperCase();
+
+		return `${numero} - ${aula}`
+
+	}
+})
