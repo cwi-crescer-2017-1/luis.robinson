c6879079e1a62b1d4da502d7d175a6b11bc956e5
Luís Robinson <luis.robinson@cwi.com.br>
2017-05-25 03:59:47 -0300

    feat(Demonstrativo): implementar função para imprimir no console.

--- modulo6_c#/LuisRobinson/LuisRobinson/Demonstrativo.cs
+++ modulo6_c#/LuisRobinson/LuisRobinson/Demonstrativo.cs
@@ -20,16 +20,55 @@ namespace FolhaPagamento
             double totalLiquido,
             Desconto fgts)
         {
-            SalarioBase = salarioBase;
+            SalarioBase = Math.Round(salarioBase, 2);
             HrsConvencao = hrsConvencao;
             HorasExtras = horasExtras;
             HorasDescontadas = horasDescontadas;
-            TotalProventos = totalProventos;
+            TotalProventos = Math.Round(totalProventos, 2);
             Inss = inss;
             Irrf = irrf;
-            TotalDescontos = totalDescontos;
-            TotalLiquido = totalLiquido;
+            TotalDescontos = Math.Round(totalDescontos, 2);
+            TotalLiquido = Math.Round(totalLiquido, 2);
             Fgts = fgts;
+        }
+
+        public void ImprimeConsole()
+        {
+
+            /*
+             * +---------------------------------------------------+
+               -DEMONSTRATIVO DE FOLHA DE PAGAMENTO (CONTRA-CHEQUE)-
+               +---------------------------------------------------+
+               -Salário Mensalista (200hrs)                  5000,00
+               -Horas Extras (50hrs)                         1250,00
+               -Horas Descontadas (10hrs)                     250,00
+               -Total de Proventos                           6000,00
+               -INSS (10%)                                    600,00
+               -IRRF (27,5%)                                 1485,00
+               -Total de Descontos                           2085,00
+               -Total Líquido                                3915,00
+               -FGTS (11%)                                    660,00
+               +---------------------------------------------------+
+               -----------------------FIM---------------------------
+               +---------------------------------------------------+
+             * */
+
+            Console.WriteLine("+---------------------------------------------------+");
+            Console.WriteLine("-DEMONSTRATIVO DE FOLHA DE PAGAMENTO (CONTRA-CHEQUE)-");
+            Console.WriteLine("+---------------------------------------------------+");
+            Console.WriteLine($"-Salário Mensalista               { SalarioBase}");
+            Console.WriteLine($"-Horas Extras                     {HorasExtras.CalcHoras()}");
+            Console.WriteLine($"-Horas Descontadas                {HorasDescontadas.CalcHoras()}");
+            Console.WriteLine($"-Total de Proventos               {TotalProventos}");
+            Console.WriteLine($"-INSS                             {Inss.CalcDesc()}");
+            Console.WriteLine($"-IRRF                             {Irrf.CalcDesc()}");
+            Console.WriteLine($"-Total de Descontos               {TotalDescontos}");
+            Console.WriteLine($"-Total Líquido                    { TotalLiquido}");
+            Console.WriteLine($"-FGTS                             {Fgts.CalcDesc()}");            
+            Console.WriteLine("+---------------------------------------------------+");
+            Console.WriteLine("-----------------------FIM---------------------------");
+            Console.WriteLine("+---------------------------------------------------+");            
+
         }
 
         public double SalarioBase { get; private set; }
@@ -43,4 +82,6 @@ namespace FolhaPagamento
         public double TotalLiquido { get; private set; }
         public Desconto Fgts { get; private set; }
     }
+
+
 }
