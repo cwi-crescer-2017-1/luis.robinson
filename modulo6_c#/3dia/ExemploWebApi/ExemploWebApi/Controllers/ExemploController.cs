e42a25de5d3b504a82d938ec73552fa95d4ca4b9
Luís Robinson <luis.robinson@cwi.com.br>
2017-05-26 16:26:37 -0300

    Feat(exemplosAPI): implementar exemplos vistos em sala de aula.

---
+++ modulo6_c#/3dia/ExemploWebApi/ExemploWebApi/Controllers/ExemploController.cs
@@ -0,0 +1,28 @@
+﻿using ExemploWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Http;
+
+namespace ExemploWebApi.Controllers
+{
+    // app start tem um arquivo com os comandos
+    // Exemplo,pois o controller no final é automático
+    public class ExemploController : ApiController
+    {
+        public HttpResponseMessage Get(string cor)
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+            response.Content = new StringContent($"<html><body style=\"background-color:{cor};color: white;\">Hello World</body></html>");
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("text/html");
+            return response;
+        }
+    }
+
+
+
+
+}
---
+++ modulo6_c#/3dia/ExemploWebApi/ExemploWebApi/Controllers/HeroisController.cs
@@ -0,0 +1,36 @@
+﻿using ExemploWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ExemploWebApi.Controllers
+{
+    public class HeroisController : ApiController
+    {
+        private static List<Heroi> Herois = new List<Heroi>();
+        private static int IdGenerator = 0;
+        private readonly object objetoLock = new object();
+
+        public List<Heroi> GetHerois(int? id = null)
+        {
+            return id == null ? Herois : Herois.Where(h => h.Id == id).ToList();
+        }
+
+        public IHttpActionResult Post(Heroi heroi)
+        {
+            if (heroi == null)
+            {
+                return BadRequest();
+            }
+            lock (objetoLock)
+            {
+                heroi.Id = IdGenerator++;
+                Herois.Add(heroi);
+            }
+            return Ok();
+        }
+    }
+}
