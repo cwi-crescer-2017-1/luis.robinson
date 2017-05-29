f47a961125d082b2ff3516abce5cb51f2c4628a5
Luís Robinson <luis.robinson@cwi.com.br>
2017-05-29 14:10:12 -0300

    refactor(Controller):remover funções não utilizadas.

--- modulo6_c#/Chat/frontend/js/ChatController.js
+++ modulo6_c#/Chat/frontend/js/ChatController.js
@@ -2,24 +2,8 @@ var modulo = angular.module('chatCwi', ['ngRoute']);
 
 modulo.controller('ChatController', function ($scope, heroisService) {
     $scope.herois = {};
-    $scope.enviarHeroi = enviarHeroi;
-    obterHerois();
-
-    function obterHerois() {
-        heroisService
-        .obterHerois()
-        .then(response => {
-            $scope.herois = response.data;
-        })
-    }
-
-    function enviarHeroi() {
-        heroisService
-        .enviarHeroi()
-        .then(herois => {
-            obterHerois();
-        })
-    }
+    $scope.enviarMensagem = enviarMensagem;
+    obterMensagem();
 
     function obterMensagem() {
         heroisService
--- modulo6_c#/Chat/frontend/js/LoginController.js
+++ modulo6_c#/Chat/frontend/js/LoginController.js
@@ -2,24 +2,8 @@ var modulo = angular.module('chatCwi', ['ngRoute']);
 
 modulo.controller('LoginController', function ($scope, heroisService) {
     $scope.herois = {};
-    $scope.enviarHeroi = enviarHeroi;
-    obterHerois();
-
-    function obterHerois() {
-        heroisService
-        .obterHerois()
-        .then(response => {
-            $scope.herois = response.data;
-        })
-    }
-
-    function enviarHeroi() {
-        heroisService
-        .enviarHeroi()
-        .then(herois => {
-            obterHerois();
-        })
-    }
+    $scope.enviarLogin = enviarLogin;
+    obterLogin();
 
     function obterLogin() {
         heroisService
