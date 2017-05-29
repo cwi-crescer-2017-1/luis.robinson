a21d3212b3324004c82d842faab26dbd508855c2
Luís Robinson <luis.robinson@cwi.com.br>
2017-05-29 10:15:34 -0300

    feat(service): implementar novas rotas.

--- modulo6_c#/Chat/frontend/js/service.js
+++ modulo6_c#/Chat/frontend/js/service.js
@@ -1,4 +1,30 @@
-modulo.factory("heroisService", function ($http) {
+modulo.factory("loginService", function ($http) {
+    return ({
+        obterHerois: obterHerois,
+        enviarHeroi: enviarHeroi
+    });
+
+    function obterHerois() {
+        return $http.get("http://localhost:24206/api/herois");
+    }
+
+    function enviarHeroi() {
+        return $http({
+            method: "post",
+            url: "http://localhost:24206/api/herois",
+            data: {
+                "Id": 0,
+                "Nome": "Giovani",
+                "Poder": {
+                    "Nome": "Threads",
+                    "Dano": 9999
+                }
+            }
+        });
+    }
+});
+
+modulo.factory("chatService", function ($http) {
     return ({
         obterHerois: obterHerois,
         enviarHeroi: enviarHeroi
