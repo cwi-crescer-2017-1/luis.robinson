109da937bcff2c4b64c62688b9d5eb26f81952c9
Luís Robinson <luis.robinson@cwi.com.br>
2017-05-29 17:25:00 -0300

    feat(Exe4): implementar exemplos do dia 4, ADO com WebApi.

---
+++ modulo6_c#/4dia/Demo1/Demo1.WebApi/Controllers/ProdutosController.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Demo1.Dominio.Entidades;
+using Demo1.Infraestrutura.Repositorios;
+
+namespace Demo1.WebApi.Controllers
+{
+    public class ProdutosController : ApiController
+    {
+        ProdutoRepositorio _produtoRepositorio = new ProdutoRepositorio();
+        //criar produto
+        public IHttpActionResult Post(Produto produto)
+        {
+
+            var mensagens = new List<string>();
+            if (produto.Validar(out mensagens))
+                return BadRequest(string.Join(".", mensagens.ToArray()));
+
+             
+                //return Ok(string.Empty);
+                  return Ok(produto);
+        }
+
+        public IHttpActionResult Get(Produto produto)
+        {
+            var produtos = _produtoRepositorio.Listar();
+            //return Ok(string.Empty);
+            return Ok(produto);
+        }
+    }
+}
