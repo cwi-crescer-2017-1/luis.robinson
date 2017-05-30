b6861a661c8fade41da497ae46420f576e676182
Luís Robinson <luis.robinson@cwi.com.br>
2017-05-30 10:25:52 -0300

    Feat(ValidaçãoPedido): validar se o produto do pedido tem estoque.

--- modulo6_c#/4dia/Demo1/Demo1.Dominio/Entidades/Pedido.cs
+++ modulo6_c#/4dia/Demo1/Demo1.Dominio/Entidades/Pedido.cs
@@ -11,5 +11,20 @@ namespace Demo1.Dominio.Entidades
         public int Id { get; set; }
         public string NomeCliente { get; set; }
         public List<ItemPedido> Itens { get; set; }
+
+        public bool Validar(out List<string> mensagens)
+        {
+            mensagens = new List<string>();
+
+            // concatena mensagem de erro.
+            foreach (var Item in Itens) {
+                if(Item.Quantidade < 1)
+                {
+                    mensagens.Add($"{Item.ProdutoId} sem estoque.");
+                }
+            }
+
+            return mensagens.Count() == 0;
+        }
     }
 }
