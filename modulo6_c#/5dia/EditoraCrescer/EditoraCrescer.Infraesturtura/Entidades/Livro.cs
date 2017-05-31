56c3035cba5029770556ca9c587ef76d27b831fd
Luís Robinson <luis.robinson@cwi.com.br>
2017-05-31 01:23:18 -0300

    Fix(NomeRevisor):ajustar nome de propriedade.

--- modulo6_c#/5dia/EditoraCrescer/EditoraCrescer.Infraesturtura/Entidades/Livro.cs
+++ modulo6_c#/5dia/EditoraCrescer/EditoraCrescer.Infraesturtura/Entidades/Livro.cs
@@ -15,7 +15,7 @@ namespace EditoraCrescer.Infraesturtura.Entidades
         public DateTime DataPublicacao { get; set; }
         public int IdAutor { get; set; }
         public Autor Autor { get; set; }
-        public int IdReviso { get; set; }
+        public int IdRevisor { get; set; }
         public Revisor Revisor { get; set; }
         public DateTime DataRevisao { get; set; }
     }
