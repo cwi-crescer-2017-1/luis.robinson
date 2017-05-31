769d84117b149ab493721a5002255e4704a6a88a
Luís Robinson <luis.robinson@cwi.com.br>
2017-05-31 01:24:39 -0300

    Feat(AutorRepositorio): implementar classe de autor.

---
+++ modulo6_c#/5dia/EditoraCrescer/EditoraCrescer.Infraesturtura/Repositorios/AutorRepositorio.cs
@@ -0,0 +1,33 @@
+﻿using EditoraCrescer.Infraesturtura.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditoraCrescer.Infraesturtura.Repositorios
+{
+    public class AutorRepositorio
+    {
+        private Contexto contexto = new Contexto();
+
+        public List<Autor> Listar()
+        {
+            return contexto.Autores.ToList();
+        }
+
+        public void Salvar(Autor autor)
+        {
+            contexto.Autores.Add(autor);
+            contexto.SaveChanges();
+        }
+
+        public Autor Excluir(int id)
+        {
+            var autorExcluido = contexto.Autores.FirstOrDefault(a => a.Id == id);
+            contexto.Autores.Remove(autorExcluido);
+            contexto.SaveChanges();
+            return autorExcluido;
+        }
+    }
+}
