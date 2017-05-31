41e61c78579f53260eb6a4076509f6e543c2ba2a
Luís Robinson <luis.robinson@cwi.com.br>
2017-05-31 01:46:00 -0300

    Feat(Repositorio):adicionar o repositório de revisor.

---
+++ modulo6_c#/5dia/EditoraCrescer/EditoraCrescer.Infraesturtura/Repositorios/RevisorRepositorio.cs
@@ -0,0 +1,33 @@
+﻿using EditoraCrescer.Infraesturtura.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditoraCrescer.Infraesturtura.Repositorios
+{
+    class RevisorRepositorio
+    {
+        private Contexto contexto = new Contexto();
+
+        public List<Revisor> Listar()
+        {
+            return contexto.Revisores.ToList();
+        }
+
+        public void Salvar(Revisor revisor)
+        {
+            contexto.Revisores.Add(revisor);
+            contexto.SaveChanges();
+        }
+
+        public Revisor Excluir(int id)
+        {
+            var revisorExcluido = contexto.Revisores.FirstOrDefault(a => a.Id == id);
+            contexto.Revisores.Remove(revisorExcluido);
+            contexto.SaveChanges();
+            return revisorExcluido;
+        }
+    }
+}
