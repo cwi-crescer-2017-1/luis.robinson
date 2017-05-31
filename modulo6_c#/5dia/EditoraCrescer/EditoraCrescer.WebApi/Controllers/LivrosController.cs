628c319830e01b6939bd7917f2b5afa4d0ab991b
Luís Robinson <luis.robinson@cwi.com.br>
2017-05-31 17:06:07 -0300

    Feat(controller): adicionar novas implementações de rotas.

--- modulo6_c#/5dia/EditoraCrescer/EditoraCrescer.WebApi/Controllers/LivrosController.cs
+++ modulo6_c#/5dia/EditoraCrescer/EditoraCrescer.WebApi/Controllers/LivrosController.cs
@@ -9,24 +9,72 @@ using System.Web.Http;
 
 namespace EditoraCrescer.WebApi.Controllers
 {
+    [RoutePrefix("api/Livros")]
+
     public class LivrosController : ApiController
     {
-        private LivroRepositorio repositorio = new LivroRepositorio();
+        private readonly LivroRepositorio repositorio = new LivroRepositorio();
 
+        [HttpGet]
         public IHttpActionResult Get()
         {
-            return Ok(repositorio.Listar());
+            var livros = repositorio.Obter();
+
+            return Ok(new { dados = livros });
         }
 
+        [HttpGet]
+        [Route("{isbn:int}")]
+        public IHttpActionResult Get(int isbn)
+        {
+            var livros = repositorio.ObterPorIsbn(isbn);
+
+            return Ok(new { dados = livros });
+        }
+
+        [HttpGet]
+        [Route("{genero:string}")]
+        public IHttpActionResult Get(string genero)
+        {
+            var livros = repositorio.ObterPorGenero(genero);
+
+            return Ok(new { dados = livros });
+        }
+
+        [HttpPost]
         public IHttpActionResult Post(Livro livro)
         {
-            repositorio.Salvar(livro);
+            repositorio.Criar(livro);
             return Ok();
         }
 
-        public IHttpActionResult Delete(int id)
+        [HttpDelete]
+        [Route("{isbn:int}")]        
+        public IHttpActionResult Remove(int isbn)
         {
-            return Ok(repositorio.Excluir(id));
+            return Ok(repositorio.Delete(isbn));
+        }
+
+        
+        
+        public IHttpActionResult Alterar(int isbn, Livro livro)
+        {
+            if (isbn == livro.Isbn)
+            {
+                repositorio.Alterar(livro);
+                return Ok();
+            }
+            else {
+                return BadRequest("Sem id correto, sem livro.");
+            }
+            
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            repositorio.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
+
