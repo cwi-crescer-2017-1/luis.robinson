e54ebe2b6cafd6976022a5edc0bb3d0b8bd56e29
Luís Robinson <luis.robinson@cwi.com.br>
2017-05-31 01:54:23 -0300

    Feat(RevisoresController): implementar controller de revisor.

---
+++ modulo6_c#/5dia/EditoraCrescer/EditoraCrescer.WebApi/Controllers/RevisoresController.cs
@@ -0,0 +1,32 @@
+﻿using EditoraCrescer.Infraesturtura.Entidades;
+using EditoraCrescer.Infraesturtura.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EditoraCrescer.WebApi.Controllers
+{
+    public class RevisoresController : ApiController
+    {
+        private RevisorRepositorio repositorio = new RevisorRepositorio();
+
+        public IHttpActionResult Get()
+        {
+            return Ok(repositorio.Listar());
+        }
+
+        public IHttpActionResult Post(Revisor revisor)
+        {
+            repositorio.Salvar(revisor);
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            return Ok(repositorio.Excluir(id));
+        }
+    }
+}
