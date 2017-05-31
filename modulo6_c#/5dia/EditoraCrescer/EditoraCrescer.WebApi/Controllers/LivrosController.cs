667b1eae0a897bc74b516dd8cde875795ddd62a1
Luís Robinson <luis.robinson@cwi.com.br>
2017-05-31 01:25:39 -0300

    Feat(LivrosController): adicionar Controller de livros.

---
+++ modulo6_c#/5dia/EditoraCrescer/EditoraCrescer.WebApi/Controllers/LivrosController.cs
@@ -0,0 +1,31 @@
+﻿using EditoraCrescer.Infraesturtura.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EditoraCrescer.WebApi.Controllers
+{
+    public class LivrosController : ApiController
+    {
+        private LivroRepositorio repositorio = new LivroRepositorio();
+
+        public IHttpActionResult Get()
+        {
+            return Ok(repositorio.Listar());
+        }
+
+        public IHttpActionResult Post(Livro livro)
+        {
+            repositorio.Salvar(livro);
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            return Ok(repositorio.Excluir(id));
+        }
+    }
+}
