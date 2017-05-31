ee83b8d98ffbb9cade9454b395d47259bcabc553
Luís Robinson <luis.robinson@cwi.com.br>
2017-05-31 17:03:53 -0300

    Feat(LivroRepositorio):  Implementar novos métodos.

--- modulo6_c#/5dia/EditoraCrescer/EditoraCrescer.Infraesturtura/Repositorios/LivroRepositorio.cs
+++ modulo6_c#/5dia/EditoraCrescer/EditoraCrescer.Infraesturtura/Repositorios/LivroRepositorio.cs
@@ -1,35 +1,65 @@
 ﻿using EditoraCrescer.Infraesturtura.Entidades;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EditoraCrescer.Infraesturtura.Repositorios
 {
-    public class LivroRepositorio
+    public class LivroRepositorio : IDisposable
     {
-        private Contexto contexto = new Contexto();
+        private Contexto contexto;
 
         public LivroRepositorio()
-        { }
+        {
+            contexto = new Contexto();
+        }
 
-        public List<Livro> Listar()
+        public List<Livro> Obter()
         {
             return contexto.Livros.ToList();
         }
 
-        public void Salvar(Livro livro)
+        public List<Livro> ObterPorIsbn(int isbn)
+        {
+            return contexto.Livros.Where(x => x.Isbn == isbn).ToList();
+        }
+
+        public List<Livro> ObterPorGenero(string genero)
+        {
+            return contexto.Livros.Where(x => x.Genero == genero).ToList();
+        }
+
+        
+
+        public void Criar(Livro livro)
         {
             contexto.Livros.Add(livro);
             contexto.SaveChanges();
         }
-
-        public Livro Excluir(int id)
+        public Livro Delete(int isbn)
         {
-            var livroRemovido = contexto.Livros.FirstOrDefault(l => l.Isbn == id);
-            contexto.Livros.Remove(livroRemovido);
-            return livroRemovido;
+            Livro livroExcluido = contexto.Livros.FirstOrDefault(x => x.Isbn == isbn);
+            contexto.Livros.Remove(livroExcluido);
+            contexto.SaveChanges();
+            return livroExcluido;
         }
+
+        public void Alterar(Livro livro)
+        {
+            //Livro livro = contexto.Livros.FirstOrDefault(x => x.Isbn == isbn);
+            contexto.Entry(livro).State = EntityState.Modified;
+            contexto.SaveChanges();            
+        }
+
+        public void Dispose()
+        {
+            contexto.Dispose();
+            //base.Dispose(disposing);
+        }
+            
     }
-}
+        
+  }
