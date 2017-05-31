cab6d070d57e8597d32a721268bd85d7791a5ff1
Luís Robinson <luis.robinson@cwi.com.br>
2017-05-31 01:54:45 -0300

    Feat(AutoresController): implementar controller de autor.

---
+++ modulo6_c#/5dia/EditoraCrescer/EditoraCrescer.WebApi/Controllers/AutoresController.cs
@@ -0,0 +1,32 @@
+﻿using EditoraCrescer.Infraesturtura.Entidades;
+using EditoraCrescer.Infraesturtura.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EditoraCrescer.WebApi.Controllers
+{
+    public class AutoresController : ApiController
+    {
+        private AutorRepositorio repositorio = new AutorRepositorio();
+
+        public IHttpActionResult Get()
+        {
+            return Ok(repositorio.Listar());
+        }
+
+        public IHttpActionResult Post(Autor autor)
+        {
+            repositorio.Salvar(autor);
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            return Ok(repositorio.Excluir(id));
+        }
+    }
+}
