2ca89db2226abe8163afd90d6f796f05a52190b7
Luís Robinson <luis.robinson@cwi.com.br>
2017-05-31 01:25:13 -0300

    Feat(LivroRepositorio): implementar classe repositório de livro.

---
+++ modulo6_c#/5dia/EditoraCrescer/EditoraCrescer.Infraesturtura/Repositorios/LivroRepositorio.cs
@@ -0,0 +1,35 @@
+﻿using EditoraCrescer.Infraesturtura.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditoraCrescer.Infraesturtura.Repositorios
+{
+    public class LivroRepositorio
+    {
+        private Contexto contexto = new Contexto();
+
+        public LivroRepositorio()
+        { }
+
+        public List<Livro> Listar()
+        {
+            return contexto.Livros.ToList();
+        }
+
+        public void Salvar(Livro livro)
+        {
+            contexto.Livros.Add(livro);
+            contexto.SaveChanges();
+        }
+
+        public Livro Excluir(int id)
+        {
+            var livroRemovido = contexto.Livros.FirstOrDefault(l => l.Isbn == id);
+            contexto.Livros.Remove(livroRemovido);
+            return livroRemovido;
+        }
+    }
+}
