9f64743ae31641d264c686e542773d11e7c245de
Luís Robinson <luis.robinson@cwi.com.br>
2017-05-31 17:30:02 -0300

    Feat(Arquivos de config).

--- modulo6_c#/5dia/EditoraCrescer/EditoraCrescer.Infraesturtura/Migrations/201705312027486_CriacaoDoBanco2.Designer.cs
+++ modulo6_c#/5dia/EditoraCrescer/EditoraCrescer.Infraesturtura/Migrations/201705312027486_CriacaoDoBanco2.Designer.cs
@@ -7,13 +7,13 @@ namespace EditoraCrescer.Infraesturtura.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class CriacaoDoBanco1 : IMigrationMetadata
+    public sealed partial class CriacaoDoBanco2 : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(CriacaoDoBanco1));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(CriacaoDoBanco2));
         
         string IMigrationMetadata.Id
         {
-            get { return "201705310439545_CriacaoDoBanco1"; }
+            get { return "201705312027486_CriacaoDoBanco2"; }
         }
         
         string IMigrationMetadata.Source
