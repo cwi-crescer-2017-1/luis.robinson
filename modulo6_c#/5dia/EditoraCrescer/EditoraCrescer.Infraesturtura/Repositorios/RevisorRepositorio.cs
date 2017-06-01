7ee661537439de5541e6ad3d577751331a27b129
Luís Robinson <luis.robinson@cwi.com.br>
2017-06-01 10:39:52 -0300

    feat(crud): implementar crud revisor.

--- modulo6_c#/5dia/EditoraCrescer/EditoraCrescer.Infraesturtura/Repositorios/RevisorRepositorio.cs
+++ modulo6_c#/5dia/EditoraCrescer/EditoraCrescer.Infraesturtura/Repositorios/RevisorRepositorio.cs
@@ -1,19 +1,28 @@
 ﻿using EditoraCrescer.Infraesturtura.Entidades;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EditoraCrescer.Infraesturtura.Repositorios
 {
-    public class RevisorRepositorio
+    public class RevisorRepositorio : IDisposable
     {
         private Contexto contexto = new Contexto();
 
         public List<Revisor> Listar()
         {
             return contexto.Revisores.ToList();
+        }
+
+        public Revisor ObterPorId(int id)
+        {
+            var revisor = contexto.Revisores.FirstOrDefault(x => x.Id == id);
+            contexto.Revisores.Add(revisor);
+            contexto.SaveChanges();
+            return revisor;
         }
 
         public void Salvar(Revisor revisor)
@@ -28,6 +37,19 @@ namespace EditoraCrescer.Infraesturtura.Repositorios
             contexto.Revisores.Remove(revisorExcluido);
             contexto.SaveChanges();
             return revisorExcluido;
+        }
+
+        public Revisor Alterar(int id, Revisor revisorModificado)
+        {
+            var revisor = contexto.Revisores.FirstOrDefault(x => x.Id == id);
+            contexto.Entry(revisorModificado).State = EntityState.Modified; 
+            contexto.SaveChanges();
+            return contexto.Revisores.FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Dispose()
+        {
+            contexto.Dispose();
         }
     }
 }
