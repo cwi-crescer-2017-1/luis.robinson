ee998b656b36195cadf2e843865cdc5b65cf3105
Luís Robinson <luis.robinson@cwi.com.br>
2017-06-01 10:36:22 -0300

    Feat(Crud): implementar crud de autorRepositório.

--- modulo6_c#/5dia/EditoraCrescer/EditoraCrescer.Infraesturtura/Repositorios/AutorRepositorio.cs
+++ modulo6_c#/5dia/EditoraCrescer/EditoraCrescer.Infraesturtura/Repositorios/AutorRepositorio.cs
@@ -1,19 +1,28 @@
 ﻿using EditoraCrescer.Infraesturtura.Entidades;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EditoraCrescer.Infraesturtura.Repositorios
 {
-    public class AutorRepositorio
+    public class AutorRepositorio : IDisposable
     {
         private Contexto contexto = new Contexto();
 
         public List<Autor> Listar()
         {
             return contexto.Autores.ToList();
+        }
+
+        public Autor ObterPorId(int id)
+        {
+            Autor autor = contexto.Autores.FirstOrDefault(x => x.Id == id);
+            contexto.Autores.Add(autor);
+            contexto.SaveChanges();
+            return autor;
         }
 
         public void Salvar(Autor autor)
@@ -28,6 +37,19 @@ namespace EditoraCrescer.Infraesturtura.Repositorios
             contexto.Autores.Remove(autorExcluido);
             contexto.SaveChanges();
             return autorExcluido;
+        }
+
+        public Autor Alterar(int id, Autor autorModificado)
+        {
+            var autor = contexto.Autores.FirstOrDefault(x => x.Id == id);
+            contexto.Entry(autorModificado).State = EntityState.Modified; 
+            contexto.SaveChanges();
+            return contexto.Autores.FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Dispose()
+        {
+            contexto.Dispose();
         }
     }
 }
