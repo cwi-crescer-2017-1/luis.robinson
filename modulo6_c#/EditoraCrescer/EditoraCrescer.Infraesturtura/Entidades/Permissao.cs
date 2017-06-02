b03faec6c1587b71983ef5f191cd43e1d00faa59
Luís Robinson <luis.robinson@cwi.com.br>
2017-06-02 17:22:49 -0300

    feat(permissões): implementar permissão e contole de usuário.

---
+++ modulo6_c#/EditoraCrescer/EditoraCrescer.Infraesturtura/Entidades/Permissao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditoraCrescer.Infraesturtura.Entidades
+{
+    public class Permissao
+    {
+        public string Nome { get; private set; }
+
+        public Permissao(string nome)
+        {
+            Nome = nome;
+        }
+    }
+}
